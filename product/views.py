06a4c28e43b3422e377d5669916ca33030c12983
turtas8 <turatbek.imankulov@gmail.com>
2022-10-13 14:11:50 +0600

    tuf

--- product/views.py
+++ product/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import permissions, response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from . import serializers
+from .models import Product
+from .permissions import IsAuthor
 
-# Create your views here.
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return serializers.ProductListSerializer
+        return serializers.ProductDetailSerializer
+
+    def get_permissions(self):
+        if self.action in ('update', 'partial_update', 'destroy'):
+            return [permissions.IsAuthenticated(), IsAuthor()]
+        return [permissions.IsAuthenticatedOrReadOnly()]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
